dcd82fac368fdb9abad0e941b3a52a15ee2b7704
mariafilippa <pc@PCs-MacBook-Air.local>
2018-08-09 12:23:42 +0800

    update multiples

--- multiples.py
+++ multiples.py
@@ -6,7 +6,10 @@ def get_multiples(base, limit):
         curr += base
     return set(multiples)
 
+def get_union_sum(set1, set2):
+    return sum(set1 | set2)
+
 if __name__ == "__main__":
     multiples_3 = get_multiples(3, 1000)
     multiples_5 = get_multiples(5, 1000)
-    print(sum(multiples_3 | multiples_5))
+    print(get_union_sum(multiples_3, multiples_5))
